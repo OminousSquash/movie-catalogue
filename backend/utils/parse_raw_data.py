2473c01f490e8f0eae35937e927f0b9e1cd35ac4
OminousSquash <zcabvsr@ucl.ac.uk>
2026-02-03 12:53:27 +0000

    Started processing of imdb dataset

--- backend/utils/parse_raw_data.py
+++ backend/utils/parse_raw_data.py
@@ -1,15 +1,61 @@
 from collections import defaultdict
 import pandas as pd
 import csv
+from typing import List
 
+# urls
 title_basics_path = "./datasets/IMDb/raw/title.basics.tsv"
 title_ratings_path = "./datasets/IMDb/raw/title.ratings.tsv"
+title_crew_path = "./datasets/IMDb/raw/title.crew.tsv"
+title_principals_path = "./datasets/IMDb/raw/title.principals.tsv"
+name_basics_path = "./datasets/IMDb/raw/name.basics.tsv"
 
-title_basics_fields = []
-title_basics_data = defaultdict()
-title_ratings_fields = []
+filtered_movies_data_path = "./datasets/IMDb/filtered/movies.tsv"
+filtered_genres_data_path = "./datasets/IMDb/filtered/genres.tsv"
+filtered_movies_genres_data_path = "./datasets/IMDb/filtered/movies_genres.tsv"
+
+
+# raw data
 title_ratings_data = []
-top_rated_movies = []
+title_basics_data = defaultdict()
+title_crew_data = defaultdict()
+title_principals_data = defaultdict()
+name_basics_data = defaultdict()
+
+# filtered data
+filtered_movies = []
+filtered_characters = []
+filtered_crew = []
+
+def extract_relevant_data(rows: List[dict]):
+    result = []
+    print(rows[0].keys())
+    for row in rows:
+        movie_fields = ["tconst", "primaryTitle", "isAdult", "avgRating", "numVotes", "genres"]
+        movie = {k: row[k] for k in movie_fields}
+        result.append(movie)
+    return result
+
+def load_principals_data():
+    with open(title_principals_path, newline="", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="\t")
+        title_principal_fields = next(reader)
+        print(title_principal_fields)
+        for row in reader:
+            zipped_data = dict(zip(title_principal_fields, row))
+            if zipped_data["tconst"] not in title_principals_data:
+                title_principals_data[zipped_data["tconst"]] = []
+            title_principals_data[zipped_data["tconst"]].append(zipped_data)
+
+def load_name_basics_data():
+    with open(name_basics_path, newline="", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="\t")
+        name_basics_fields = next(reader)
+        print(name_basics_fields)
+        for row in reader:
+            zipped_data = dict(zip(name_basics_fields, row))
+            name_basics_data[zipped_data["nconst"]] = zipped_data
+
 
 def load_data():
     global title_basics_fields, title_ratings_fields
@@ -32,21 +78,81 @@ def load_data():
             zipped_data["numVotes"] = int(zipped_data["numVotes"])
             zipped_data["ratingScore"] = zipped_data["averageRating"] * zipped_data["numVotes"]
             title_ratings_data.append(zipped_data) 
+    
+    with open(title_crew_path, newline="", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="\t")
+        title_crew_fields = next(reader)
+        print(title_crew_fields)
+        for row in reader:
+            zipped_data = dict(zip(title_crew_fields, row))
+            title_crew_data[zipped_data["tconst"]] = zipped_data
 
 def collect_top_rated_movies():
-    global title_ratings_data, top_rated_movies
+    global title_ratings_data
 
     title_ratings_data.sort(
         key=lambda x: (x["ratingScore"]),
         reverse=True
     )
+    top_rated_movies = []
+    print("Getting top rated")
     for i in range(len(title_ratings_data)):
         id = title_ratings_data[i]["tconst"]
         basics_data = title_basics_data[id]
+        merged = title_ratings_data[i] | title_basics_data[id]
         if basics_data["titleType"] == "movie":
-            top_rated_movies.append(basics_data)
+            top_rated_movies.append(merged)
         if len(top_rated_movies) == 10000:
             break
+    result = extract_relevant_data(top_rated_movies)
+    return result
+
+def write_to_tsv(rows, path):
+    if not rows:
+        print(f"No rows to write for {path}")
+        return
+
+    fieldnames = rows[0].keys()
+
+    with open(path, "w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter="\t")
+        writer.writeheader()
+        writer.writerows(rows)
+
+    print(f"Wrote {len(rows)} rows to {path}")
+
+def split_tables(rows:List[dict]):
+    movies_fields = ["tconst", "primaryTitle", "isAdult", "avgRating", "numVotes"]
+    movies = []
+    genres_set = set()
+    genres_list = []
+    genre_to_id = defaultdict()
+    movies_genres = []
+    for row in rows:
+        movies.append({k:row[k] for k in movies_fields})
+        genres_list = row["genres"].split(",")
+    
+    for i, g in enumerate(genres_set):
+        genres_list.append({"genreId": i, "genre": g})
+        genre_to_id[g] = i
+    
+    for row in rows:
+        movie_id = row["tconst"]
+        genres = row["genres"].split(",")
+        for genre in genres:
+            movies_genres.append({
+                "tconst":  movie_id,
+                "genreId": genre_to_id[genre]
+            })
+    
+    return movies, genres, movies_genres 
 
 load_data()
-collect_top_rated_movies()
+top_10k_movies = collect_top_rated_movies()
+movies, genres, movies_genres = split_tables(top_10k_movies)
+write_to_tsv(movies, filtered_movies_data_path)
+write_to_tsv(genres, filtered_genres_data_path)
+write_to_tsv(movies_genres, filtered_movies_genres_data_path)
+
+
+
