f1c5086267700c25659a3ceaf58ae03508358de8
OminousSquash <zcabvsr@ucl.ac.uk>
2026-02-07 12:06:16 +0000

    Created the tables for contributors, popular_movies and join table between movies and contributors

--- database/createDB.sql
+++ database/createDB.sql
@@ -20,11 +20,11 @@ CREATE TABLE IF NOT EXISTS movie_genres(
     PRIMARY KEY (tconst, genreID)
 );
 
-CREATE TABLE IF NOT EXISTS stars (
+CREATE TABLE IF NOT EXISTS contributors (
     nconst VARCHAR(10) NOT NULL PRIMARY KEY,
     primaryName VARCHAR(255) NOT NULL,
     birthYear INT NOT NULL,
-    deathYear INT NOT NULL,
+    deathYear INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS popular_works(
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS popular_works(
     PRIMARY KEY(nconst, tconst)
 );
 
-CREATE TABLE IF NOT EXISTS movie_stars(
+CREATE TABLE IF NOT EXISTS movie_contributors (
     tconst VARCHAR(10) NOT NULL,
     nconst VARCHAR(10) NOT NULL,
     role   VARCHAR(10) NOT NULL,
@@ -61,3 +61,23 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (tconst, genreID);
 
+LOAD DATA INFILE '/datasets/IMDb/filtered/contributors.tsv'
+INTO TABLE contributors
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(nconst, primaryName, birthYear, deathYear);
+
+LOAD DATA INFILE '/datasets/IMDb/filtered/movie_contributors.tsv'
+INTO TABLE movie_contributors
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(tconst, nconst, role);
+
+LOAD DATA INFILE '/datasets/IMDb/filtered/popular_works.tsv'
+INTO TABLE popular_works
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(nconst, tconst);
