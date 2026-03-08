20fd4a92ab68cbcd65bddb671cb35c58eb48244f
Nigel Mathews <zcabnm0@ucl.ac.uk>
2026-03-08 07:38:42 +0000

    added genre popularity over time endpoint

--- backend/controllers/trend_analytics_controller.py
+++ backend/controllers/trend_analytics_controller.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Query, Depends, FastAPI
-from database.services.trend_analysis_service import get_trend_analytics_service, get_contributor_trends_service
+from database.services.trend_analysis_service import get_trend_analytics_service, get_contributor_trends_service, get_genre_popularity_over_time_service
 from backend.DTOs.genre_contributor_trend_analysis_dto import GenreContributorTrendAnalysisDTO
 from database.database import get_db
  
@@ -20,3 +20,12 @@ def get_contributor_analysis(
         db=db,
         genre_contributor_dto=genre_contributor_dto
     )
+
+@router.get("/genre_popularity_over_time")
+def genre_popularity_over_time(
+    start_year: int | None = Query(None, description="Filter from this year"),
+    end_year: int | None = Query(None, description="Filter up to this year"),
+    genres: list[str] | None = Query(None, description="List of genres to include"),
+    db = Depends(get_db)
+):
+    return get_genre_popularity_over_time_service(db, start_year, end_year, genres)
