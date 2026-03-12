6f46735b4557776badb5bc9f5e0a5e42eaab1aa2
Edw00765 <edw00765@gmail.com>
2026-03-12 16:08:24 +0000

    added the update route, will complete later

--- backend/controllers/fetch_update_app_user_details_controller.py
+++ backend/controllers/fetch_update_app_user_details_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Query, Depends, FastAPI
 from database.database import get_db
 from backend.DTOs.store_personality_dto import StorePersonalityDTO
 from database.services.fetch_app_user_detail_service import fetch_app_user_detail_service
+from database.services.update_app_user_detail_service import update_app_user_detail_service
 from backend.security.dependencies import get_current_user
 
 router = APIRouter(prefix="/account", tags=["account handling"])
@@ -12,5 +13,12 @@ def fetch_details(
     db = Depends(get_db),
     current_user = Depends(get_current_user)
 ):
-    fetch_app_user_detail_service(current_user=current_user, db=db)
-    update
+    return fetch_app_user_detail_service(current_user=current_user, db=db)
+
+@router.post("/update")
+def fetch_details(
+    personality_dto: StorePersonalityDTO,
+    db = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
+    return update_app_user_detail_service(current_user=current_user, db=db)
