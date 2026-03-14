3d248b1106aa58bde2b94559a8ae97eeab424675
Nigel Mathews <zcabnm0@ucl.ac.uk>
2026-03-14 12:35:16 +0000

    redis added to popularity report

--- database/services/popularity_service.py
+++ database/services/popularity_service.py
@@ -1,5 +1,7 @@
 from mysql.connector import Error, MySQLConnection
 from fastapi import HTTPException, status
+from backend.utils.redis_client import redis_client
+import json
 
 PAGE_SIZE = 50
 
@@ -8,6 +10,11 @@ def get_popularity_report_service(
     db: MySQLConnection,
 ):
     try:
+        cache_key = "popularity_report"
+        cached_result = redis_client.get(cache_key)
+        if cached_result:
+            return json.loads(cached_result)
+
         cursor = db.cursor(dictionary = True)
         popularity_query = """
             SELECT 
@@ -22,6 +29,10 @@ def get_popularity_report_service(
             GROUP BY g.genre
         """
         cursor.execute(popularity_query)
-        return cursor.fetchall()
+        result = cursor.fetchall()
+
+        redis_client.set(cache_key, json.dumps(result), ex=3600)
+        return result
+
     except Error:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
